84cffa71a7d3131b49015aeba465ba3720a13199
Edgard da Cunha Pontes <edgard.pontes@serpro.gov.br>
2024-06-01 10:47:28 -0300

    Adicionando a interface IConta e as classes Conta, Banco, ContaCorrente e ContaPoupanca.

--- banco-digital-java-oo/src/Main.java
+++ banco-digital-java-oo/src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Cliente cliente = new Cliente();
         cliente.setNome("Fulano de Tal");
-        System.out.println(cliente.getNome());
+
+        Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+        cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
