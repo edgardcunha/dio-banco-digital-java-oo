8b18691123196a70a680012b75e1bb75761a7e57
Edgard da Cunha Pontes <edgard.pontes@serpro.gov.br>
2024-06-01 10:34:34 -0300

    Adicionando a classe Cliente.

---
+++ banco-digital-java-oo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Fulano de Tal");
+        System.out.println(cliente.getNome());
+    }
+}
